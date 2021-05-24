0bad4754db4260fbf2f0532e80b018077676e4bb
Biren Saud <brensaud4@gmail.com>
2021-05-24 11:31:21 +0545

    Allauth completed

--- config/urls.py
+++ config/urls.py
@@ -20,10 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User management
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('users.urls')),
-
+    path('accounts/', include('allauth.urls')),
     
 
     # Local apps
