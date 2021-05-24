fe4c11429d8154be10f42cec3d4ea484641c9da2
Biren Saud <brensaud4@gmail.com>
2021-05-24 12:01:04 +0545

    Environment setup finished

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2^dfiapv45pt-n92g&on&_6v8mjq+mt+j_*mu*)-sd9p%2om6)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
